53346819634d81f1573918c1889bb1927ab80f0b
bestCoder <127156447+stish085@users.noreply.github.com>
2025-07-21 03:56:28 +0300

    Add files via upload

---
+++ 17.  3 задание.cpp
@@ -0,0 +1,51 @@
+﻿// 17.  3 задание.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+bool substr( char* a,  char* b)
+{
+    int i = 0;
+    int j = 0;
+  
+    while (*(a+i) != '\0')
+    {
+        if (*(b + j) != '\0')
+        {
+            if (*(a + i) == *(b + j))
+            {
+
+                i++;
+                j++;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        else
+        {   
+            return true;
+            break;
+        }
+    }
+    if (*(b + j) == '\0') 
+    { 
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+   
+ }
+int main()
+{   
+     char a [] = "Hello world";
+     char b []= "wor";
+     char c []= "banana";
+     std::cout << " 1- true ::: 2-false "<< std::endl;
+     std::cout << "  " << std::endl;
+     std::cout << substr(a, b) << "| |" << substr(a, c);
+     
+}
+
